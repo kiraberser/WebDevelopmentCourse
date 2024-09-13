7f3716712e589e60bf81c88de74900bd6c15eae0
kiraberser <121148745+kiraberser@users.noreply.github.com>
2024-09-13 12:16:39 -0600

    Maping data

--- React/296-react-props-practice/src/components/App.jsx
+++ React/296-react-props-practice/src/components/App.jsx
@@ -1,29 +1,31 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+
+function createCard(contact){
+  return (
+    <Card 
+      key={contact.id}
+      id={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+    
+  )
+}
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card 
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
+      <Avatar 
+        img={"https://imgs.search.brave.com/3l2TYFmyu4f6TrBDGtgnnt-3C9yksgONpdt5ecdDRjM/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9yYXJl/LWdhbGxlcnkuY29t/L3VwbG9hZHMvcG9z/dHMvNTEwNTg3LWJl/cnNlcmsuanBn"}
       />
-      <Card 
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card 
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      />
+      {contacts.map(createCard)}
     </div>
   );
 }
---
+++ React/296-react-props-practice/src/components/Avatar.jsx
@@ -0,0 +1,13 @@
+function Avatar(props) {
+  return (
+    <div>
+      <img
+        className="circle-img"
+        src={props.img}
+        alt="avatar_img"
+        />
+    </div>
+  )
+}
+
+export default Avatar
