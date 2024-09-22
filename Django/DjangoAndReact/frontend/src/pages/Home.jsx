e41f03a6cbb1ee2135f9635857a0f3f18d8a0607
kiraberser <121148745+kiraberser@users.noreply.github.com>
2024-09-22 00:55:02 -0600

    FrontendOrganization&Axios/ProtectedRoutes/Navigatos&Pages

---
+++ Django/DjangoAndReact/frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      Homess
+    </div>
+  )
+}
+
+export default Home
---
+++ Django/DjangoAndReact/frontend/src/pages/NoteFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NoteFound() {
+  return (
+    <div>
+      Not Found
+    </div>
+  )
+}
+
+export default NoteFound
