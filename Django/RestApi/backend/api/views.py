68d102976c7617d8c0ef1c17ee9b726968937a31
kiraberser <121148745+kiraberser@users.noreply.github.com>
2024-10-29 18:49:49 -0600

    model to dict

--- Django/RestApi/backend/api/views.py
+++ Django/RestApi/backend/api/views.py
@@ -1,22 +1,17 @@
 import json
 from django.http import JsonResponse
-
+from products.models import Product
 # Django Models
 
 def api_home(request, *args, **kwargs):
-    #request -> HttpRequest -> Django
-    #request.body 
-    print(request.GET)#url query params
-    print(request.POST)
-    body = request.body#byte string of json data
-    data = {}
-    try:
-        data = json.loads(body) #string of Json data -> Python Dictionary
-    except:
-        pass
-    print(data)
-    #data['headers'] = request.headers
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
+    model_data = Product.objects.all().order_by("?").first()
+    data= {}
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
+        #model instance (model_data)
+        #turn a Python dict
+        #return JSON to my client
     return JsonResponse(data)
--- Django/RestApi/frontend/basic.py
+++ Django/RestApi/frontend/basic.py
@@ -5,7 +5,7 @@ import requests
 endpoint = "http://localhost:8000/api/"
 
 
-get_response = requests.get(endpoint, params={'product_id': 123}, json={"query": "Hello world"})#Http Request
+get_response = requests.get(endpoint, json={"query": "Hello world"})#Http Request
 #print(get_response.text)
 #print(get_response.status_code)
 
