3addd94fa58b828a970e2cb9ea02322b5bd4cd4c
kiraberser <121148745+kiraberser@users.noreply.github.com>
2024-10-29 18:59:52 -0600

    Django models to dictionary

--- Django/RestApi/backend/api/views.py
+++ Django/RestApi/backend/api/views.py
@@ -1,17 +1,14 @@
 import json
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse #HttpResponse is equal to text/html
 from products.models import Product
+from django.forms.models import model_to_dict
 # Django Models
 
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data= {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-        #model instance (model_data)
-        #turn a Python dict
-        #return JSON to my client
+        data = model_to_dict(model_data, fields=['id', 'price']) #best practice 
     return JsonResponse(data)
+    #    json_data_str = json.dumps(data)
+    #return HttpResponse(json_data_str, headers={"content-type": "application/json"})
