5a1c52794c3dc7ca250693d2cdf4f9d4893560b5
kiraberser <121148745+kiraberser@users.noreply.github.com>
2024-10-29 18:13:48 -0600

    ApiRequest and Django Models

---
+++ Django/RestApi/backend/api/views.py
@@ -0,0 +1,22 @@
+import json
+from django.http import JsonResponse
+
+# Django Models
+
+def api_home(request, *args, **kwargs):
+    #request -> HttpRequest -> Django
+    #request.body 
+    print(request.GET)#url query params
+    print(request.POST)
+    body = request.body#byte string of json data
+    data = {}
+    try:
+        data = json.loads(body) #string of Json data -> Python Dictionary
+    except:
+        pass
+    print(data)
+    #data['headers'] = request.headers
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
---
+++ Django/RestApi/frontend/basic.py
@@ -0,0 +1,13 @@
+import requests
+
+#endpoint = "https://httpbin.org/status/200/"
+#endpoint = "http://localhost:8000/"
+endpoint = "http://localhost:8000/api/"
+
+
+get_response = requests.get(endpoint, params={'product_id': 123}, json={"query": "Hello world"})#Http Request
+#print(get_response.text)
+#print(get_response.status_code)
+
+#Javascript object nototion JSON -> Python Dict
+print(get_response.json())
