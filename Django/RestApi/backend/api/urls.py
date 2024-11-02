5d37e3d588ccf3b66da8c46fd0f09a8711934c46
kiraberser <121148745+kiraberser@users.noreply.github.com>
2024-11-01 22:27:06 -0600

    Token Authentication

--- Django/RestApi/backend/api/urls.py
+++ Django/RestApi/backend/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 
 from . import views 
 #from . views import api_home
 
 urlpatterns = [
+    path('auth/', obtain_auth_token),
     path('', views.api_home)
 ]
--- Django/RestApi/frontend/create.py
+++ Django/RestApi/frontend/create.py
@@ -1,5 +1,9 @@
 import requests
 
+headers = {
+    'Authorization': 'Bearer 4f175b68fcba2ab4121b2bd3a0e5eadb019ac46f'
+}
+
 endpoint = "http://localhost:8000/api/products/"
 
 data = {
@@ -7,6 +11,6 @@ data = {
     'price': 30.00
 }
 
-get_response = requests.post(endpoint, json=data)#Http Request
+get_response = requests.post(endpoint, json=data, headers=headers)#Http Request
 
 print(get_response.json(), get_response.status_code)
