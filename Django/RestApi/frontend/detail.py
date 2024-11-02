455444522b19d4fa9cb9e5a462d8cb33f084041f
kiraberser <121148745+kiraberser@users.noreply.github.com>
2024-11-02 01:44:05 -0600

    Using Mixins for Permissions

--- Django/RestApi/frontend/detail.py
+++ Django/RestApi/frontend/detail.py
@@ -1,6 +1,19 @@
 import requests
+from getpass import getpass
 
-product_id = input("What's the id of the product?\n")
+auth_endpoint = "http://localhost:8000/api/auth/"
+username = input("What is your username?\n")
+password = getpass("What is your password?\n")
+product_id = input("What is the id of the product?")
+
+auth_response = requests.post(auth_endpoint, json={'username': username, 'password': password })
+print(auth_response.json())
+
+if auth_response.status_code == 200:
+    token = auth_response.json()['token']
+    headers = {
+        'Authorization': f"Bearer {token}"
+    }
 try:
     product_id = int(product_id)
 except:
@@ -9,5 +22,5 @@ except:
     
 if product_id:
     endpoint = f"http://localhost:8000/api/products/{product_id}/"
-    get_response = requests.get(endpoint)#Http Request
+    get_response = requests.get(endpoint, headers=headers)#Http Request
     print(get_response.json())
