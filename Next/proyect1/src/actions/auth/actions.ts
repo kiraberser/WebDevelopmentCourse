6eaeb70bd926156b2d053b7a8c9c302f6856d366
kiraberser <121148745+kiraberser@users.noreply.github.com>
2025-05-29 23:27:57 -0600

    Login controller

--- Next/proyect1/src/actions/auth/actions.ts
+++ Next/proyect1/src/actions/auth/actions.ts
@@ -1,34 +1,65 @@
 'use server'
 
-import { SignupFormSchema, FormState } from "@/lib/definitions";
-import { createSession, deleteSession } from "@/lib/session";
+import axios from "axios";
+import { schemaLogin } from "@/schema/form.model";
+import { FormValuesSignup, FormValuesLogin } from "@/schema/form.model";
+import { cookies } from "next/headers";
 
-import { redirect } from "next/navigation";
-import { hash } from "bcryptjs";
-import { v4 as uuidv4 } from 'uuid';
-import { FormValues } from "@/schema";
+const API_URL = process.env.NEXT_PUBLIC_API_USER_URL
 
-    export async function signup( formData: FormValues) {
-    const validatedFields = SignupFormSchema.safeParse(formData)
-    console.log('validatedFields', validatedFields)
-
-    if(!validatedFields.success) {
-        return{
-            errors: validatedFields.error.flatten().fieldErrors
-        }
+export async function signup( formData: FormValuesSignup) {
+    try {
+        const {name, email, password} = formData
+        const username=name.split(' ').join('').toLowerCase()
+        const response = await axios.post(`${API_URL}/signup/`, { username, email, password}, {withCredentials: true})
+        console.log('response', response)
+        return response.data
+    } catch (error) {
+        console.error('Error al registrar usuario:', error)
+        return { error: 'Error al registrar usuario' }
     }
-
-    const {name, email, password} = validatedFields.data
-    console.log('name', name)
-    const userId = uuidv4()
-    const hashedPassword = await hash(password, 10)
-
-    await createSession(name)
-
-    redirect('/profile')
+}
+export const login = async (formData: FormValuesLogin) => {
+    const validatedFields = schemaLogin.safeParse(formData)
+    if(!validatedFields.success) {
+        return Promise.reject({
+            type: 'validation',
+            errors: validatedFields.error.flatten().fieldErrors,
+        });
+    }
+    const {email, password} = validatedFields.data
+    try {
+        const response = await axios.post(`${API_URL}/login/`, 
+            { email, password }, 
+            { withCredentials: true});
+        if (response.status === 200) {
+            const cookieStore = await cookies()
+            cookieStore.set('access_token', response.data.token.access_token)
+            cookieStore.set('refresh_token', response.data.token.refresh_token)
+            return response.status;
+        } else {
+            return Promise.reject({
+                 type: 'server',
+                error: response.data || 'Unknown server error',
+            });
+        }
+    } catch (error: any) {
+        console.error('Login error:', error.response?.data || error.message);
+        return Promise.reject({
+            type: 'server',
+            error: error.response?.data || 'Unknown server error',
+        });
+    }
 }
 
 export async function logout() {
-    await deleteSession()
-    redirect('/')
+    const cookieStore = await cookies()
+    cookieStore.delete('access_token')
+    cookieStore.delete('refresh_token')
+    const response = await axios.post(`${API_URL}/logout/`, {withCredentials: true})
+    return response.data
   }
+
+// Obtener perfil del usuario por ID
+
+// Obtener lista de todos los usuarios
