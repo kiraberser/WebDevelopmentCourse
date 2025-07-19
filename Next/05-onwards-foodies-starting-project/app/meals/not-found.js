bf0662a6621792b37d0412856c6970ccc7f47d8b
kiraberser <121148745+kiraberser@users.noreply.github.com>
2025-07-19 00:56:13 -0600

    Nout found and Error

---
+++ Next/05-onwards-foodies-starting-project/app/meals/not-found.js
@@ -0,0 +1,12 @@
+
+
+const NotFoundPage = () => {
+    return (
+        <main className="not-found">
+            <h1>Meal not Found</h1>
+            <p>We couldn't find the page you're looking for your meal data.</p>
+        </main>
+    )
+}
+
+export default NotFoundPage
---
+++ Next/05-onwards-foodies-starting-project/app/meals/share/error.js
@@ -0,0 +1,12 @@
+"use client"
+
+function MealsError() {
+  return (
+    <main className="error">
+      <h1>An error ocurred!</h1>
+      <p>Failed to create meal.</p>
+    </main>
+  )
+}
+
+export default MealsError
