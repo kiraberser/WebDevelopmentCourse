f2a1fb7168fcdf9c3867f8e08e52c51c83aaa184
kiraberser <121148745+kiraberser@users.noreply.github.com>
2025-07-19 01:02:58 -0600

    Dynamic Metadata

--- Next/05-onwards-foodies-starting-project/app/meals/[slug]/page.jsx
+++ Next/05-onwards-foodies-starting-project/app/meals/[slug]/page.jsx
@@ -1,11 +1,49 @@
-const MealsSlugPage = ({params}) => {
-    return(
-        <main>
-            <div>
-                <h1>Meals Slug</h1>
-                <p>{params.slug}</p>
-            </div>
-        </main>
+import { getMeal } from "@/lib/meals"
+import styles from "./page.module.css"
+
+import Image from "next/image"
+import { notFound } from "next/navigation"
+
+export const generateMetadata = async ({params}) => {
+    const meal = getMeal(params.slug)
+    
+    if (!meal) {
+        notFound()
+    }
+    
+    return{
+        title: meal.title,
+        description: meal.summary
+    }
+}
+
+const MealsSlugPage = ({ params }) => {
+    const meal = getMeal(params.slug)
+
+    if (!meal) {
+        notFound()
+    }
+
+    meal.instructions = meal.instructions.replace(/\n/g, '<br/>')
+    
+    return (
+        <>
+            <header className={styles.header}>
+                <div className={styles.image}>
+                    <Image src={meal.image} alt={meal.title} fill />
+                </div>
+                <div className={styles.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={styles.creator}>{meal.creator} by <a href={`malito:${meal.creator_email}`}>{meal.creator}</a></p>
+                    <p className={styles.summary}>{meal.summary}</p>
+                </div>
+            </header>
+            <main>
+                <p
+                    className={styles.instructions}
+                    dangerouslySetInnerHTML={{ __html: meal.instructions }}></p>
+            </main>
+        </>
     )
 }
 
