1fd133f43a8314fa79d3b0e9ff6fa2abe904e3b5
kiraberser <121148745+kiraberser@users.noreply.github.com>
2025-07-19 01:04:05 -0600

    Saving images xss slugify fs by node:fs and Metadata

--- Next/05-onwards-foodies-starting-project/app/meals/page.jsx
+++ Next/05-onwards-foodies-starting-project/app/meals/page.jsx
@@ -5,6 +5,11 @@ import styles from './page.module.css'
 import { getMeals } from "@/lib/meals"
 import { Suspense } from "react"
 
+export const metadata = {
+    title: "All Meals",
+    description: "Browse the delicious meals shared by our vibrant community."
+}
+
 const Meals = async () => {
     const meals = await getMeals();
     return <MealGrid meals={meals}/>
--- Next/05-onwards-foodies-starting-project/lib/meals.js
+++ Next/05-onwards-foodies-starting-project/lib/meals.js
@@ -1,4 +1,8 @@
 import sql from 'better-sqlite3';
+import slugify from 'slugify';
+import xss from 'xss';
+
+import fs from 'node:fs'
 
 const db = sql('meals.db');
 
@@ -7,5 +11,42 @@ export const getMeals = async () => {
     //throw new Error('Something went wrong')
     const meals = db.prepare('SELECT * FROM meals').all();
     return meals;
+}
+
+export const getMeal = (slug) => {
+    const meal = db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug)
+    return meal
+}
+
+export const saveMeal = async (meal) => {
+    meal.slug = slugify(meal.title, { lower: true })
+    meal.instructions = xss(meal.instructions)
+
+    const extension = meal.image.name.split('.').pop()
+    const fileName = `${meal.slug}.${extension}`
+
+    const stream = fs.createWriteStream(`public/images/${fileName}`)
+    const bufferedImage = await meal.image.arrayBuffer()
+
+    stream.write(Buffer.from(bufferedImage), (error) => {
+        if (error) {
+            throw new Error('Savinf image failed')
+        }
+    })
+
+    meal.image = `/images/${fileName}`
+
+    db.prepare(
+        `INSERT INTO meals (title, summary, instructions, image, creator, creator_email, slug) 
+        VALUES (
+            @title, 
+            @summary, 
+            @instructions, 
+            @image, 
+            @creator, 
+            @creator_email,
+            @slug 
+            )`
+    ).run(meal)
 }
 
