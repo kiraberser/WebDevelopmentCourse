67e8ee159edae3000e711712c72411e035aa2afa
kiraberser <121148745+kiraberser@users.noreply.github.com>
2025-07-19 00:54:57 -0600

    Using useStateForm to use a better ux

---
+++ Next/05-onwards-foodies-starting-project/components/meals-form-submit.js
@@ -0,0 +1,11 @@
+"use client"
+import { useFormStatus } from "react-dom"
+
+const MealsFormSubmit = () => {
+    const { pending } = useFormStatus()
+    return (
+        <button disabled={pending}>{pending ? 'Submitting...' : 'Submit'}</button>
+    )
+}
+
+export default MealsFormSubmit
